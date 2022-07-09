55bdbf3f438d9cc0d3cfa1002eaad22a8a33796c
b0pez <blopez4434@gmail.com>
2022-07-08 22:57:17 -0400

    routes

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,10 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectMongoDB from './config/connectDB.js';
 
+// Importing routes
+import veterinary from './routes/veterinary.js';
+
 const app = express();
 dotenv.config();
 
 connectMongoDB();
+
+
+//routes
+app.use('/api/veterinary', veterinary);
+
+//middleware
+app.use((req, res, next) => {
+    res.status(404).send('not found 404');
+});
 
 const PORT = process.env.PORT || 4000;
 
